e0e05e09b79673a9e822adeeb05b9cd778dfd92a
KLOER1 <97724653+KLOER1@users.noreply.github.com>
2024-11-07 19:10:54 +0500

    Add files via upload

---
+++ HomeWork.Tests/HomeWorkTests.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HomeWork.Tests
+{
+    [TestClass]
+    public class HomeWorkTests
+    {
+        [TestMethod]
+        public void BaseClass_Constructor_CorrectValues_Test()
+        {
+            // Arrange
+            string expected = "The point has coodinates (0, 0, 0)";
+
+            // Act
+            Point point = new Point("0", "0", "0");
+            string actual = point.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BaseClass_Constructor_IncorrectValues_Test()
+        {
+            // Arrange
+            string expected = "The point has coodinates (0, 0, 0)";
+
+            // Act
+            Point point = new Point("This", "Incorrect", "Values");
+            string actual = point.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ChildClass_Constructor_CorrectValues_Test()
+        {
+            // Arrange
+            string expected = "The point has coodinates (0, 0, 0)\n" +
+                $"The modulus of the vector is equal to 0\n" +
+                $"The angle between the vector and the Ox plane is 0°\n" +
+                $"The angle between the vector and the Oy plane is 0°\n" +
+                $"The angle between the vector and the Oz plane is 0°"; ;
+
+            // Act
+            PointOn3D point = new PointOn3D("0", "0", "0");
+            string actual = point.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ChildClass_Constructor_IncorrectValues_Test()
+        {
+            // Arrange
+            string expected = "The point has coodinates (0, 0, 0)\n" +
+                $"The modulus of the vector is equal to 0\n" +
+                $"The angle between the vector and the Ox plane is 0°\n" +
+                $"The angle between the vector and the Oy plane is 0°\n" +
+                $"The angle between the vector and the Oz plane is 0°";
+
+            // Act
+            PointOn3D point = new PointOn3D("This", "Incorrect", "Values");
+            string actual = point.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BaseClass_MethodMultiplication_Test1()
+        {
+            // Arrange
+            double expected = 0;
+
+            // Act
+            Point point = new Point("124523,312412", "412342142,524634324", "0");
+            double actual = point.Multiplication();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BaseClass_MethodMultiplication_Test2()
+        {
+            // Arrange
+            double expected = 24233548.106235;
+
+            // Act
+            Point point = new Point("123,45", "345,67", "567,89");
+            double actual = point.Multiplication();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BaseClass_CopyConstructor_Test()
+        {
+            // Arrange
+            string expected = "The point has coodinates (4523, 1535, 6246)";
+            Point FirstPoint = new Point("4523", "1535", "6246");
+
+            // Act
+            Point ResultPoint = new Point(FirstPoint);
+            string actual = ResultPoint.ToString();
+
+            // Assert
+            Assert.AreEqual(expected , actual);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+    }
+}
---
+++ HomeWork.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HomeWork.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeWork.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56e79239-cbc1-4b36-89f0-ab70707eaad9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
